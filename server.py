4fd211524793c3eebd2976ab33c423df14fb57eb
Dmitry <dvg1995@gmail.com>
2018-01-25 13:37:46 +0300

    Add prototypes of check_db and start methods

--- server.py
+++ server.py
@@ -4,10 +4,12 @@
 import zmq
 import asyncio
 import zmq.asyncio
+from db import Publication
+import peewee
 
 
 class Server:
-    def __init__(self, port):
+    def __init__(self, port: str):
         self.ctx = zmq.asyncio.Context.instance()
         self.socket_sub_pub = self.ctx.socket(zmq.PUB)
         self.socket_sub_pub.bind("tcp://*:%s" % port)
@@ -17,6 +19,23 @@ class Server:
     def __del__(self):
         print(self.socket_server_client.close())
         print(self.socket_sub_pub.close())
+
+    def check_db(self):
+        """
+        Check unposted publications in database.
+
+        :return:
+        """
+        try:
+            Publication.create_table()  # Try to create publication table in db
+        except peewee.IntegrityError:
+            pass
+        need_to_post = [publication for publication in Publication.select().where(Publication.it_posted == False)]
+        if len(need_to_post) > 0:
+            pass  # TODO: Handle unposted publications.
+
+    def start(self):
+        self.check_db()
 
 
 if __name__ == "__main__":
